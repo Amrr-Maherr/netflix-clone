d9c4e98545aa874f0e55f8afd789e38684d1ecf4
Amrr-Maherr <amrr.maherr242gmail.com>
2025-09-11 23:44:53 +0300

    update

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -7,73 +7,67 @@ export default function Hero() {
   return (
     <>
       <Header />
-      <FadeContent
-        className="bg-black"
-        blur={true}
-        duration={1000}
-        easing="ease"
-        initialOpacity={0}
+      <section
+        className="relative h-screen w-full bg-cover bg-center"
+        style={{ backgroundImage: "url('/images/bg_image.jpg')" }}
       >
-        <section
-          className="relative h-screen w-full bg-cover bg-center"
-          style={{ backgroundImage: "url('/images/bg_image.jpg')" }}
-        >
-          <div className="absolute inset-0 bg-gradient-to-t from-black via-black/60 to-black/60"></div>
+        <div className="absolute inset-0 bg-gradient-to-t from-black via-black/60 to-black/60"></div>
 
-          <div className="relative z-10 flex flex-col items-center justify-center h-full px-4 sm:px-6 md:px-12 text-center">
-            <SplitText
-              text="Unlimited movies, TV shows, and more"
-              className="text-white text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-extrabold drop-shadow-lg max-w-3xl"
-              delay={100}
-              duration={0.6}
-              ease="power3.out"
-              splitType="chars"
-              from={{ opacity: 0, y: 40 }}
-              to={{ opacity: 1, y: 0 }}
-              threshold={0.1}
-              rootMargin="-100px"
-              textAlign="center"
-            />
-            <SplitText
-              text="Starts at EGP 100. Cancel anytime."
-              className="text-white/80 mt-4 sm:mt-6 max-w-xl text-base sm:text-lg md:text-xl leading-relaxed drop-shadow"
-              delay={100}
-              duration={0.6}
-              ease="power3.out"
-              splitType="chars"
-              from={{ opacity: 0, y: 40 }}
-              to={{ opacity: 1, y: 0 }}
-              threshold={0.1}
-              rootMargin="-100px"
-              textAlign="center"
-            />
-            <SplitText
-              text="Ready to watch? Enter your email to create or restart your
+        <div className="relative z-10 flex flex-col items-center justify-center h-full px-4 sm:px-6 md:px-12 text-center">
+          <SplitText
+            text="Unlimited movies, TV shows, and more"
+            className="text-white text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-extrabold drop-shadow-lg max-w-3xl"
+            delay={100}
+            duration={0.6}
+            ease="power3.out"
+            splitType="chars"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin="-100px"
+            textAlign="center"
+          />
+          <SplitText
+            text="Starts at EGP 100. Cancel anytime."
+            className="text-white/80 mt-4 sm:mt-6 max-w-xl text-base sm:text-lg md:text-xl leading-relaxed drop-shadow"
+            delay={100}
+            duration={0.6}
+            ease="power3.out"
+            splitType="chars"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin="-100px"
+            textAlign="center"
+          />
+          <SplitText
+            text="Ready to watch? Enter your email to create or restart your
               membership."
-              className="text-white/80 mt-4 sm:mt-6 max-w-xl text-base sm:text-lg md:text-xl leading-relaxed drop-shadow"
-              delay={100}
-              duration={0.6}
-              ease="power3.out"
-              splitType="chars"
-              from={{ opacity: 0, y: 40 }}
-              to={{ opacity: 1, y: 0 }}
-              threshold={0.1}
-              rootMargin="-100px"
-              textAlign="center"
-            />
-            <div className="mt-6 flex flex-col sm:flex-row items-center gap-3 sm:gap-4 w-full max-w-[830px]">
+            className="text-white/80 mt-4 sm:mt-6 max-w-xl text-base sm:text-lg md:text-xl leading-relaxed drop-shadow"
+            delay={100}
+            duration={0.6}
+            ease="power3.out"
+            splitType="chars"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin="-100px"
+            textAlign="center"
+          />
+          <FadeContent className="md:w-[830px] w-full">
+            <div className="mt-6 flex flex-col sm:flex-row items-center gap-3 sm:gap-4 w-full">
               <Input
                 type="email"
                 placeholder="Email address"
                 className="w-full flex-1 h-[60px] py-4 px-4 rounded-md bg-black/70 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#E50914] text-sm sm:text-base border-0"
               />
-              <Button className="bg-[#E50914] w-full md:w-fit hover:bg-[#B00710] text-white font-semibold px-5 sm:px-6 h-[60px] rounded-md transition text-sm sm:text-base">
+              <Button className="bg-[#E50914] cursor-pointer w-full md:w-fit hover:bg-[#B00710] text-white font-semibold px-5 sm:px-6 h-[60px] rounded-md transition text-sm sm:text-base">
                 Get Started &gt;
               </Button>
             </div>
-          </div>
-        </section>
-      </FadeContent>
+          </FadeContent>
+        </div>
+      </section>
     </>
   );
 }
